841a642593b5518dff76f351565ae5a89550034a
devlinkcne <linkne2012@gmail.com>
2019-11-28 14:05:39 -0300

    Task: Versão Alpha Fortaleza

--- app.js
+++ app.js
@@ -4,11 +4,38 @@ var gpio = require('rpi-gpio');
 
 gpio.setup(7, gpio.DIR_OUT);
 
+app.use(function(req, res, next){
+
+	res.setHeader("Access-Control-Allow-Origin", "*");
+	res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+	res.setHeader("Access-Control-Allow-Headers", "content-type");
+	res.setHeader("Access-Control-Allow-Credentials", true);
+
+	next();
+});
+
 app.get('/actCancel', function(req, res){
-gpio.write(7, true, function(err) {
-        if (err) throw err;
-        console.log('Cancela Liberada');
-    });
+	gpio.write(7, true, function(err) {
+		if (err) throw err;
+		for(var a = 0; a < 10; a ++){
+			for(var i = 0; i < 10; i++){
+				gpio.setup(7, gpio.DIR_OUT);
+				gpio.write(7,true);
+				gpio.setup(7, gpio.DIR_OUT);
+				gpio.write(7,false);
+				gpio.setup(7, gpio.DIR_IN);
+			}
+			for(var i = 0; i < 10; i++){
+				gpio.setup(7, gpio.DIR_OUT);
+				gpio.write(7,true);
+				gpio.setup(7, gpio.DIR_OUT);
+				gpio.write(7,false);
+				gpio.setup(7, gpio.DIR_IN);
+			}	
+		}
+		console.log("OK");
+		res.send('{"msg":"OK"}');
+	    });
 
 });
 
