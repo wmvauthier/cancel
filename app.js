a8e7e10352beae2463fbc6b65e6e1ae8d62263d8
devlinkcne <linkne2012@gmail.com>
2019-11-29 14:11:30 -0300

    Task: Melhorias da Versao Alpha

--- app.js
+++ app.js
@@ -1,10 +1,29 @@
-var express = require('express'); 
+var express = require('express');
 var app = express();
 var gpio = require('rpi-gpio');
 
 gpio.setup(7, gpio.DIR_OUT);
 
-app.use(function(req, res, next){
+function actCancel(gpio) {
+	for (var a = 0; a < 10; a++) {
+		for (var i = 0; i < 10; i++) {
+			gpio.setup(7, gpio.DIR_OUT);
+			gpio.write(7, true);
+			gpio.setup(7, gpio.DIR_OUT);
+			gpio.write(7, false);
+			gpio.setup(7, gpio.DIR_IN);
+		}
+		for (var i = 0; i < 10; i++) {
+			gpio.setup(7, gpio.DIR_OUT);
+			gpio.write(7, true);
+			gpio.setup(7, gpio.DIR_OUT);
+			gpio.write(7, false);
+			gpio.setup(7, gpio.DIR_IN);
+		}
+	}
+}
+
+app.use(function (req, res, next) {
 
 	res.setHeader("Access-Control-Allow-Origin", "*");
 	res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
@@ -12,33 +31,20 @@ app.use(function(req, res, next){
 	res.setHeader("Access-Control-Allow-Credentials", true);
 
 	next();
+	
 });
 
-app.get('/actCancel', function(req, res){
-	gpio.write(7, true, function(err) {
+app.get('/actCancel', function (req, res) {
+	gpio.write(7, true, function (err) {
+
 		if (err) throw err;
-		for(var a = 0; a < 10; a ++){
-			for(var i = 0; i < 10; i++){
-				gpio.setup(7, gpio.DIR_OUT);
-				gpio.write(7,true);
-				gpio.setup(7, gpio.DIR_OUT);
-				gpio.write(7,false);
-				gpio.setup(7, gpio.DIR_IN);
-			}
-			for(var i = 0; i < 10; i++){
-				gpio.setup(7, gpio.DIR_OUT);
-				gpio.write(7,true);
-				gpio.setup(7, gpio.DIR_OUT);
-				gpio.write(7,false);
-				gpio.setup(7, gpio.DIR_IN);
-			}	
-		}
-		console.log("OK");
-		res.send('{"msg":"OK"}');
-	    });
+		actCancel(gpio);
+		res.send('{"msg":"Cancela Liberada"}');
+
+	});
 
 });
 
 app.listen(2000, function () {
-  console.log('Bem vindo ao Cancel!')
+	console.log('Bem vindo ao Cancel!')
 })
